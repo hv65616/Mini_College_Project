41063c57e515021d361ecfb12000f964c4716a95
Himanshu <hv65616@gmail.com>
2021-12-13 02:07:31 +0530

    Code Implementation

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
-import cv2
 import numpy as np
+import cv2
 import glob
 import os
 
 logo = cv2.imread("logo.png")
-h_logo, w_logo, _ = logo.shape
+h_logo, w_logo = logo.shape[:2]
 
 images_path = glob.glob("images/*.*")
 
-print("Adding watermark")
-for img_path in images_path:
-    img = cv2.imread(img_path)
-    h_img, w_img, _ = img.shape
+print(images_path)
 
-    # Get the center of the original. It's the location where we will place the watermark
-    center_y = int(h_img / 2)
-    center_x = int(w_img / 2)
-    top_y = center_y - int(h_logo / 2)
-    left_x = center_x - int(w_logo / 2)
-    bottom_y = top_y + h_logo
-    right_x = left_x + w_logo
+print("Adding Watermark")
+for image_path in images_path:
 
-    # Get ROI
-    roi = img[top_y: bottom_y, left_x: right_x]
+    img = cv2.imread(image_path)
+    h_img, w_img = img.shape[:2]
 
-    # Add the Logo to the Roi
-    result = cv2.addWeighted(roi, 1, logo, 0.3, 0)
+    scale = 40
+    h = int(logo.shape[0]*scale/100)
+    w = int(logo.shape[1]*scale/100)
+    dim = (w, h)
+    updated_logo = cv2.resize(logo, dim, interpolation=cv2.INTER_AREA)
+    ulh, ulw = logo.shape[:2]
 
-    # Replace the ROI on the image
-    img[top_y: bottom_y, left_x: right_x] = result
+    centre_y = int(h_img/2)
+    centre_x = int(w_img/2)
+    top_y = centre_y - int(h_logo/2)
+    left_x = centre_x - int(w_logo/2)
+    bottom_y = top_y+h_logo
+    right_x = left_x+w_logo
+    # cv2.circle(img, (left_x, top_y), 30, (0, 255, 0), -1)
+    # cv2.circle(img, (right_x, bottom_y), 30, (0, 255, 0), -1)
 
-    # Get filename and save the image
-    filename = os.path.basename(img_path)
-    cv2.imwrite("images/watermarked_" + filename, img)
+    roi = img[top_y:bottom_y, left_x:right_x]
+    # img[top_y:bottom_y, left_x:right_x] = logo
+    result = cv2.addWeighted(roi, 0.5, logo, 0.5, 0)
+    img[top_y:bottom_y, left_x:right_x] = result
+    filename = os.path.basename(image_path)
+    cv2.imwrite(filename+" Watermarked", img)
+
+    # print(h_img, end=" ")
+    # print(w_img, end=" ")
+    # print(centre_y, end=" ")
+    # print(centre_x, end=" ")
+    # print(top_y, end=" ")
+    # print(left_x, end=" ")
+    # print(bottom_y, end=" ")
+    # print(right_x, end=" ")
+
+print("Watermarked Images")
+# cv2.imshow("Logo", logo)
+# cv2.imshow("Image", img)
+#
+# cv2.imshow("ROI", roi)
+#
+# cv2.imshow("Result", result)
+# cv2.waitKey(0)
